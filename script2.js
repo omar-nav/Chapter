cf949a37ea0b848b71b369373576205b17d65645
primaryuser <13755487+omar-nav@users.noreply.github.com>
2018-04-19 14:08:32 -0500

    enhance the populateInfo() function to assign values from the queryArray to the hidden fields

--- script2.js
+++ script2.js
@@ -13,8 +13,12 @@ var queryArray = [];
 function populateInfo() {
     if (location.search) {
         var queryData = location.search;
+        var hiddenInputs = document.querySelectorAll("input[type=hidden]");
         queryData = queryData.substring(1, queryData.length);
         queryArray = queryData.split("&");
+        for (var i = 0; i < queryArray.length; i++) {
+            hiddenInputs[i].value = queryArray[i].substring(queryArray[i].lastIndexOf("=") + 1)
+        }
     }
 }
 
